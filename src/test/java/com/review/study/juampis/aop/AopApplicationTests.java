e999c11a0acf61385006271e0619a9692a90b16d
Juan Pablo Montoya Ospinal <62717509+juanpablommm@users.noreply.github.com>
2024-03-25 12:21:02 -0500

    :sparkles: initial commit

---
+++ src/test/java/com/review/study/juampis/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.study.juampis.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
